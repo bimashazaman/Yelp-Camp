a02617938fed08ad001a7b86c4429b3b1486c224
bimashazaman <bimashazaman1234@gmail.com>
2023-12-03 02:48:43 +0600

    Initial model and schema and view files

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+
+const Campground = require('./models/campground')
+
+//database
+mongoose
+  .connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to DB!'))
+  .catch((error) => console.log(error.message))
+
 const app = express()
 
 //views
@@ -14,6 +27,26 @@ app.use('/img', express.static(__dirname + 'public/img'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show', { campground })
 })
 
 const port = 3000
